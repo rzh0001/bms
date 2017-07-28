51a25825771874dae105d33643e5b43e872be719
xjher <xjher@192.168.0.189>
2017-07-28 14:52:50 +0800

    swaggerui里添加公共Header参数token

--- bms-api/src/main/java/com/xj/api/config/swagger/SwaggerConfig.java
+++ bms-api/src/main/java/com/xj/api/config/swagger/SwaggerConfig.java
@@ -1,13 +1,19 @@
 package com.xj.api.config.swagger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +28,17 @@ public class SwaggerConfig {
 	 */
 	@Bean
 	public Docket createRestApi() {
+		 ParameterBuilder parameterBuilder = new ParameterBuilder();
+		 parameterBuilder
+	                .parameterType("header") //参数类型支持header, cookie, body, query etc
+	                .name("token") //参数名
+	                .defaultValue("") //默认值
+	                .description("header中token")
+	                .modelRef(new ModelRef("string"))//指定参数值的类型
+	                .required(false).build(); //非必需，这里是全局配置
+	        List<Parameter> params = new ArrayList<Parameter>();
+	        params.add(parameterBuilder.build());
+		
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())//调用apiInfo方法,创建一个ApiInfo实例,里面是展示在文档页面信息内容
 				.select()
@@ -30,7 +47,7 @@ public class SwaggerConfig {
 				//@ApiIgnore 这样,该接口就不会暴露在 swagger2 的页面下
 				.apis(RequestHandlerSelectors.basePackage("com.xj.api"))
 				.paths(PathSelectors.any())
-				.build();
+				.build().globalOperationParameters(params);
 	}
     //构建 api文档的详细信息函数
 	private ApiInfo apiInfo() {
